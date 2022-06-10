23415082aba4177979592528f782d08b6d79b3bc
eisont <rlaclgns321@naver.com>
2022-06-10 14:16:33 +0900

    skill 이미지, page3 추가

--- src/components/commons/Page3/page3.presenter.js
+++ src/components/commons/Page3/page3.presenter.js
@@ -3,7 +3,29 @@ import * as S from "./page3.styles";
 export default function Page3UI() {
   return (
     <S.Wrapper>
-      <>page3</>
+      <S.Skill>
+        <S.Title>Skill</S.Title>
+        <S.Hr />
+        <S.SkillIconBox>
+          <S.SkillIcon src="/skill/yarn.png" />
+          <S.SkillIcon src="/skill/git.png" />
+          <S.SkillIconBox>
+            <S.SkillIcon src="/skill/github.png" style={{ height: "30px" }} />
+            <S.SkillIcon
+              src="/skill/githubText.png"
+              style={{ marginLeft: "10px", height: "25px" }}
+            />
+          </S.SkillIconBox>
+          <S.SkillIcon src="/skill/npm.png" />
+          <S.SkillIcon src="/skill/docker.png" />
+          <S.SkillIcon src="/skill/gcp-logo.png" />
+          <S.SkillIcon src="/skill/html5.png" />
+          <S.SkillIcon src="/skill/css3.png" />
+          <S.SkillIcon src="/skill/nodejs.png" />
+          <S.SkillIcon src="/skill/javascript.png" />
+          <S.SkillIcon src="/skill/typescript.png" />
+        </S.SkillIconBox>
+      </S.Skill>
     </S.Wrapper>
   );
 }
--- src/components/commons/Page3/page3.styles.js
+++ src/components/commons/Page3/page3.styles.js
@@ -1,10 +1,38 @@
 import styled from "@emotion/styled";
 
 export const Wrapper = styled.div`
-  width: 100%;
+  margin: 0 auto;
+  padding: 20px;
+  width: 80%;
   height: 100vh;
   background: #fff;
   display: flex;
-  justify-content: center;
+  justify-content: centecr;
+  align-items: start;
+`;
+
+export const Title = styled.div`
+  font-size: 30px;
+  font-weight: 900;
+`;
+export const Hr = styled.div`
+  margin: 10px 0 20px 0;
+  width: 200px;
+  height: 2px;
+  border-radius: 50px;
+  background: #999;
+`;
+export const Skill = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  align-items: start;
+`;
+export const SkillIconBox = styled.div`
+  display: flex;
   align-items: center;
+`;
+export const SkillIcon = styled.img`
+  height: 50px;
 `;
